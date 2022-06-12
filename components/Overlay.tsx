95d1434e0bcb963169d0bfb85344d5ff37e9a522
Jesper Asplund <jesper.asplund@geshdo.com || jesper.asplund95@gmail.com>
2022-06-12 21:19:59 +0200

    make it responsive

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -4,7 +4,7 @@ const Overlay = () => {
   const [open, setOpen] = useState(false)
 
   if (open) {
-    return <div className="fixed z-40 inset-0" style={{ backgroundImage: 'url("/xp.jpg")', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: 'center', backgroundSize: 'cover' }}>
+    return <div className="fixed z-40 inset-0" style={{ backgroundImage: 'url("/xp.jpg")', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundPosition: '60% 50%', backgroundSize: 'cover' }}>
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="group cursor-pointer hover:border borger-white hover:opacity-90 hover:bg-gray-800 p-2" onClick={() => setOpen(false)}>
           <img width="48px" height="48px" className="mx-auto" src="/A.png" alt="..." />
